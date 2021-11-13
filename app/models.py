88ca57d39ca802e4b53d8047677d13098060e4fe
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-13 21:10:06 +0300

    add routes for authentication

--- app/models.py
+++ app/models.py
@@ -4,11 +4,11 @@ from flask_login import UserMixin
 from datetime import datetime
 
 @login_manager.user_loader
-def user_loader(user_id):
+def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model,UserMixin):
-    __tablename__="users"
+    __tablename__ = "users"
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
@@ -17,8 +17,11 @@ class User(db.Model,UserMixin):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     
+  
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -43,6 +46,9 @@ class Pitch(db.Model):
     category = db.Column(db.String(255), index = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comment = db.relationship("Comment",backref="pitch",lazy="dynamic")
+    upvote = db.relationship("Upvote",backref="pitch",lazy="dynamic")
+    downvote = db.relationship("Downvote",backref="pitch",lazy="dynamic")
+
     
     def save_pitch(self):
         db.session.add(self)
@@ -51,6 +57,43 @@ class Pitch(db.Model):
         
     def __repr__(self):
         return f'Pitch {self.post}'
+
+class Upvote(db.Model):
+    __tablename__ = "upvotes"
+
+    id = db.Column(db.Integer,primary_key=True)
+    pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
+class Downvote(db.Model):
+    __tablename__ = "downvotes"
+
+    id = db.Column(db.Integer,primary_key=True)
+    pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+   
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def get_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(pitch_id=id).all()
+        return downvote
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'
 
 
 class Comment(db.Model):
@@ -73,5 +116,3 @@ class Comment(db.Model):
     
     def __repr__(self):
         return f'comment:{self.comment}'
-
-    
