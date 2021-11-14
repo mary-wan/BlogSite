c39bacd4e48e3a1bb9c7d4fcc02e0452a3423ce2
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-14 04:08:09 +0300

    add comment model

--- app/models.py
+++ app/models.py
@@ -17,8 +17,6 @@ class User(db.Model,UserMixin):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
-    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
-    downvote = db.relationship('Downvote',backref='user',lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     
   
@@ -43,11 +41,9 @@ class Pitch(db.Model):
     title = db.Column(db.String(255))
     post = db.Column(db.Text())
     time = db.Column(db.DateTime, default = datetime.utcnow)
-    # category = db.Column(db.String(255), index = True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     comment = db.relationship("Comment",backref="pitch",lazy="dynamic")
-    upvote = db.relationship("Upvote",backref="pitch",lazy="dynamic")
-    downvote = db.relationship("Downvote",backref="pitch",lazy="dynamic")
+    
 
     
     def save_pitch(self):
@@ -58,42 +54,6 @@ class Pitch(db.Model):
     def __repr__(self):
         return f'Pitch {self.post}'
 
-class Upvote(db.Model):
-    __tablename__ = "upvotes"
-
-    id = db.Column(db.Integer,primary_key=True)
-    pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-    
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    @classmethod
-    def get_upvotes(cls,id):
-        upvote = Upvote.query.filter_by(pitch_id=id).all()
-        return upvote
-
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
-class Downvote(db.Model):
-    __tablename__ = "downvotes"
-
-    id = db.Column(db.Integer,primary_key=True)
-    pitch_id = db.Column(db.Integer,db.ForeignKey("pitches.id"))
-    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
-   
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-    @classmethod
-    def get_downvotes(cls,id):
-        downvote = Downvote.query.filter_by(pitch_id=id).all()
-        return downvote
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.pitch_id}'
 
 
 class Comment(db.Model):
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
-from app.models import User,Pitch,Upvote,Downvote,Comment
+from app.models import User,Pitch,Comment
 
 # Creating app instance
 app = create_app('development')
@@ -25,6 +25,6 @@ def test():
     
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User=User,Pitch=Pitch,Upvote=Upvote,Downvote=Downvote,Comment=Comment)
+    return dict(app = app,db = db,User=User,Pitch=Pitch,Comment=Comment)
 if __name__ == '__main__':
     manager.run()
