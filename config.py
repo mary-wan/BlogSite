02a8c8c9b5316a6f8bd402e148dc85851afde566
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-15 09:41:55 +0300

    remove comment

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:pass@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
